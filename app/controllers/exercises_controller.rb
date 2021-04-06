5b0d92525c67e1aeda920fb6c35c77f5276be05d
aa03813 <anichenko.anastasia@gmail.com>
2021-04-06 12:01:25 +0100

    Increased number of exercises returned by API

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -94,6 +94,6 @@ class ExercisesController < ApplicationController
      end
 
      def get_exercise_by_category(category)
-      return HTTParty.get('https://wger.de/api/v2/exercise/?language=2&category=' + category.to_s).parsed_response
+      return HTTParty.get('https://wger.de/api/v2/exercise/?language=2&limit=500&category=' + category.to_s).parsed_response
     end
 end
