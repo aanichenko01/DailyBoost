5b285f74a6814df6b6abbd4e68ab43585e25062f
aa03813 <anichenko.anastasia@gmail.com>
2021-04-19 13:46:22 +0100

    Fixed errors that occured running other machine

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Rails: '@rails/ujs'
-  Popper: ['popper.js', 'default']
-
-}))
-
 module.exports = environment
