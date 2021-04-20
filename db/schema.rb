b5ff0f758e0a970586b6544f0be9cddbc5d26ef8
gmichail147 <74865205+gmichail147@users.noreply.github.com>
2021-04-20 13:08:34 +0100

    fix difficulty issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_110702) do
+ActiveRecord::Schema.define(version: 2021_04_20_095532) do
 
   create_table "exercises", force: :cascade do |t|
     t.integer "workout_id", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_110702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "difficulty", default: 1, null: false
-    t.string "username"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
