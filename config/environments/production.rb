f1c5db0e16780b3a6a2e08630ff618bfbea2bd28
victor-peychev <victor.peychev@gmail.com>
2021-05-04 04:41:24 -0700

    redis deployed for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://dailyboost.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://dailyboost.herokuapp.com', 'http://dailyboost.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
